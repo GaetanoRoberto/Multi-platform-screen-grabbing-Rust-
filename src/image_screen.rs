b83362c14b66c26ec1a8667c29b8babab1cb0f2a
DOMENICO GAGLIARDO s310454 <s310454@studenti.polito.it>
2023-08-14 15:36:17 +0200

    Add Screenshot Logic

---
+++ src/image_screen.rs
@@ -0,0 +1,205 @@
+use std::fs;
+use std::fs::create_dir_all;
+use druid::{BoxConstraints, commands, Cursor, Env, Event, EventCtx, ImageBuf, LayoutCtx, LifeCycle, LifeCycleCtx, MouseButton, Screen, Size, UpdateCtx, Widget, WidgetExt, WindowDesc};
+use druid::piet::{ImageFormat, InterpolationMode};
+use druid::platform_menus::mac::file::print;
+//use druid::piet::d2d::Image;
+use druid::widget::{Button, Flex, Image};
+use image::load_from_memory_with_format;
+use crate::GrabData;
+
+pub struct ScreenshotWidget {
+    image: Image,
+}
+
+impl ScreenshotWidget {
+    pub fn new(image: Option<Image>) -> Self {
+        match image {
+            Some(value) => {
+                ScreenshotWidget {image: value}
+            }
+            None => {
+                ScreenshotWidget{image : Image::new(ImageBuf::empty())}
+            }
+        }
+    }
+}
+
+impl Widget<GrabData> for ScreenshotWidget {
+    fn event(&mut self, ctx: &mut EventCtx, event: &Event, data: &mut GrabData, _env: &Env) {
+        let mut min_x = 0;
+        let mut min_y = 0;
+        let mut max_x = 0;
+        let mut max_y = 0;
+
+        /*if let Event::WindowCloseRequested = event {
+            if !data.image_data.is_empty() {
+                let dynamic_image = load_from_memory_with_format(&data.image_data, image::ImageFormat::Png)
+                    .expect("Failed to load image from memory");
+                let rgba_image = dynamic_image.to_rgba8();
+                let image_buf = ImageBuf::from_raw(
+                    rgba_image.clone().into_raw(),
+                    ImageFormat::RgbaSeparate,
+                    rgba_image.clone().width() as usize,
+                    rgba_image.clone().height() as usize,
+                );
+
+                let image = Image::new(image_buf);
+                let screen = screenshots::Screen::all().unwrap()[data.monitor_index];
+                let rect = druid::Screen::get_monitors()[data.monitor_index].virtual_rect();
+                //println!("{:?}",rect);
+                //println!("{:?}",screen);
+                ctx.window().close();
+                ctx.new_window(
+                    WindowDesc::new(
+                        Flex::<GrabData>::column()
+                            .with_child(ScreenshotWidget::new(Some(image))))
+                        .set_position((rect.x0,rect.y0))
+                        .window_size((300.0,300.0)));
+            }
+        }*/
+
+        if let Event::MouseDown(_) = event {
+            data.press = true;
+            //ctx.set_cursor(&Cursor::Crosshair);
+            println!("prova");
+        }
+
+        if let Event::MouseMove(mouse_event) = event {
+            ctx.set_cursor(&Cursor::Crosshair);
+            if data.press {
+                data.positions.push((mouse_event.window_pos.x,mouse_event.window_pos.y));
+            }
+        }
+
+        if let Event::MouseUp(_) = event {
+            data.press = false;
+            //println!("{:?}",data.positions);
+            if !data.positions.is_empty() {
+                let screen = screenshots::Screen::all().unwrap()[data.monitor_index];
+
+                /*let (min_x2, min_y2) = data.positions.iter().cloned().min_by(|(x1, y1), (x2, y2)| {
+                    x1.partial_cmp(x2).unwrap()
+                }).unwrap();
+
+                let (max_x2, max_y2) = data.positions.iter().cloned().max_by(|(x1, y1), (x2, y2)| {
+                    x1.partial_cmp(x2).unwrap()
+                }).unwrap();*/
+
+                let (min_x2, max_y2) = data.positions.iter().cloned().fold(
+                    (f64::INFINITY, f64::NEG_INFINITY),
+                    |(min_x, max_y), (x, y)| (min_x.min(x), max_y.max(y)),
+                );
+
+                let (max_x2, min_y2) = data.positions.iter().cloned().fold(
+                    (f64::NEG_INFINITY, f64::INFINITY),
+                    |(max_x, min_y), (x, y)| (max_x.max(x), min_y.min(y)),
+                );
+
+                // empty positions
+                data.positions = vec![];
+
+                let scale_factor_x = ctx.scale().x();
+                let scale_factor_y = ctx.scale().y();
+                min_x = (min_x2 as f64 * scale_factor_x) as i32;
+                max_x = (max_x2 as f64 * scale_factor_x) as i32;
+                min_y = (min_y2 as f64 * scale_factor_y) as i32;
+                max_y = (max_y2 as f64 * scale_factor_y) as i32;
+
+                let mut diff_y = 0;
+                if min_y > max_y {
+                    diff_y = min_y - max_y;
+                } else {
+                    diff_y = max_y - min_y;
+                }
+                //println!("minx {} maxx {} miny {} maxy {}",min_x,max_x,min_y,max_y);
+                let image = screen.capture_area(min_x as i32, min_y as i32, (max_x - min_x) as u32, (max_y - min_y) as u32).unwrap();
+                let buffer = image.to_png(None).unwrap();
+                data.image_data = buffer;
+
+                let dynamic_image = load_from_memory_with_format(&data.image_data, image::ImageFormat::Png)
+                    .expect("Failed to load image from memory");
+                let rgba_image = dynamic_image.to_rgba8();
+                let image_buf = ImageBuf::from_raw(
+                    rgba_image.clone().into_raw(),
+                    ImageFormat::RgbaSeparate,
+                    rgba_image.clone().width() as usize,
+                    rgba_image.clone().height() as usize,
+                );
+
+                let image = Image::new(image_buf);
+
+                let save_button = Button::new("Save").on_click(move |_ctx, _data: &mut GrabData ,_env| {
+                    if !_data.image_data.is_empty() {
+                        fs::write(format!("Screen{}.{}",_data.screenshot_number,_data.save_format), _data.image_data.clone()).unwrap();
+                    }
+                    if _data.screenshot_number == u32::MAX {
+                        _data.screenshot_number = 0;
+                    } else {
+                        _data.screenshot_number+=1;
+                    }
+                    // cancel all image data
+                    _data.image_data = vec![];
+                });
+
+                let cancel_button = Button::new("Cancel").on_click(move |_ctx, _data: &mut GrabData ,_env| {
+                    // cancel all image data
+                    _data.image_data = vec![];
+                });
+                let rect = druid::Screen::get_monitors()[data.monitor_index].virtual_rect();
+                //println!("{:?}",rect);
+                //println!("{:?}",screen);
+                ctx.window().close();
+                println!("create");
+                ctx.new_window(
+                    WindowDesc::new(
+                        Flex::<GrabData>::column()
+                            .with_child(ScreenshotWidget::new(Some(image)))
+                            .with_child(
+                                Flex::row()
+                                    .with_child(save_button)
+                                    .with_child(cancel_button)
+                            ))
+                        .set_position((rect.x0,rect.y0)));
+            }
+            //fs::write(format!("Screen{}.{}",data.screenshot_number,data.save_format), data.image_data.clone()).unwrap();
+        }
+        /*if let Event::Command(cmd) = event {
+            if cmd.is(commands::HIDE_WINDOW) {
+                // Capture the screenshot
+                println!("inside");
+                let image = screenshots::Screen::all().unwrap()[data.monitor_index].capture().unwrap();
+                let buffer = image.to_png(None).unwrap();
+                data.image_data = buffer;
+
+                // Show the window using the SHOW_WINDOW command
+                let dynamic_image = load_from_memory_with_format(&vec![], image::ImageFormat::Png)
+                    .expect("Failed to load image from memory");
+                let rgba_image = dynamic_image.to_rgba8();
+                let image_buf = ImageBuf::from_raw(
+                    rgba_image.clone().into_raw(),
+                    ImageFormat::RgbaSeparate,
+                    rgba_image.clone().width() as usize,
+                    rgba_image.clone().height() as usize,
+                );
+                //let image = Image::new(image_buf);
+                ctx.submit_command(commands::SHOW_WINDOW);
+                ctx.request_paint();
+            }
+        }*/
+    }
+
+    fn lifecycle(&mut self, ctx: &mut LifeCycleCtx, event: &LifeCycle, data: &GrabData, env: &Env) {
+    }
+
+    fn update(&mut self, ctx: &mut UpdateCtx, old_data: &GrabData, data: &GrabData, env: &Env) {
+    }
+
+    fn layout(&mut self, ctx: &mut LayoutCtx, bc: &BoxConstraints, data: &GrabData, env: &Env) -> Size {
+        bc.max()
+    }
+
+    fn paint(&mut self, paint_ctx: &mut druid::PaintCtx, data: &GrabData, env: &druid::Env) {
+        self.image.paint(paint_ctx, data, env);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,54 @@
-use std::fs;
+mod image_screen;
+
+use std::{fs, thread};
 use std::fs::File;
 use std::io::Read;
-use std::path::Path;
-use druid::{ImageBuf, Application, Color, Data, Widget, LocalizedString, WindowDesc, AppLauncher, PlatformError, widget::{Image, Label, Button, Flex}, WidgetExt, AppDelegate, DelegateCtx, WindowId, piet};
+use std::ops::Deref;
+use std::path::{Path, PathBuf};
+use std::ptr::null;
+use std::sync::{Arc, Condvar, Mutex};
+use std::time::Duration;
+use druid::{commands, ImageBuf, Application, Color, Data, Widget, LocalizedString, WindowDesc, AppLauncher, PlatformError, widget::{Image, Label, Button, Flex}, WidgetExt, AppDelegate, DelegateCtx, WindowId, piet, LifeCycleCtx, LifeCycle, Env, RenderContext, Event, UpdateCtx, LayoutCtx, BoxConstraints, Size, PaintCtx, EventCtx, Rect, Scale, Point};
 use druid::piet::ImageFormat;
-use screenshots::Screen;
+use druid::platform_menus::mac::file::print;
+use screenshots::{DisplayInfo, Screen};
 use serde::{Serialize,Deserialize};
 use serde_json::{to_writer,from_reader};
 use image::{open, DynamicImage, ImageBuffer, Rgba, GenericImageView, load_from_memory_with_format};
+use serde::de::Unexpected::Str;
+use crate::image_screen::ScreenshotWidget;
 
-#[derive(Clone, Data, Serialize, Deserialize)]
-struct GrabData {
+#[derive(Clone, Data, Serialize, Deserialize, Debug)]
+pub struct GrabData {
     screenshot_number : u32,
+    monitor_index : usize,
     #[data(ignore)]
     image_data: Vec<u8>,
     #[data(ignore)]
     save_path: Box<Path>,
-    save_format : String
+    save_format : String,
+    press: bool,
+    #[data(ignore)]
+    positions: Vec<(f64,f64)>
 }
 
 fn create_monitor_buttons() -> Flex<GrabData> {
     let screens = Screen::all().unwrap();
-    let mut monitor_buttons = Flex::column();
+    let mut monitor_buttons = Flex::row();
     let mut monitor_index = 1;
     for screen in screens {
         let btn = Button::new( "📷 Monitor ".to_owned() + &monitor_index.to_string()).on_click(
             move |_ctx, _data: &mut GrabData ,_env| {
-                let image = screen.capture().unwrap();
-                let buffer = image.to_png(None).unwrap();
-                _data.image_data = buffer;
-                /* SAVE LOGIC
-                fs::write(format!("Screen{}.{}",_data.screenshot_number,_data.save_format), buffer).unwrap();
-                if _data.screenshot_number == u32::MAX {
-                    _data.screenshot_number = 0;
-                } else {
-                    _data.screenshot_number+=1;
-                }
-                SAVE LOGIC */
+                _data.monitor_index = monitor_index - 1;
+                let rect = druid::Screen::get_monitors()[_data.monitor_index].virtual_rect();
+                _ctx.window().close();
+                _ctx.new_window(
+                    WindowDesc::new(
+                        Flex::<GrabData>::row().with_child(ScreenshotWidget::new(None)))
+                        .show_titlebar(false)
+                        .transparent(true)
+                        .set_position((rect.x0,rect.y0))
+                        .window_size(Size::new(screen.display_info.width as f64,screen.display_info.height as f64)));
             }
         );
         monitor_buttons = monitor_buttons.with_child(btn);
@@ -49,8 +61,8 @@ fn build_ui() -> impl Widget<GrabData> {
     //let title = Label::new("Screen Grabbing Utility");
     /*let dynamic_image = open("C:\\Users\\Domenico\\CLionProjects\\pds_project\\Screen1.png").unwrap();
     // Create a DynamicImage from the image data buffer
-    //let dynamic_image = load_from_memory_with_format(&image_data, ImageFormat::Png)
-    //    .expect("Failed to load image from memory");
+    let dynamic_image = load_from_memory_with_format(&vec![], image::ImageFormat::Png)
+        .expect("Failed to load image from memory");
     let rgba_image = dynamic_image.to_rgba8();
     let image_buf = ImageBuf::from_raw(
         rgba_image.clone().into_raw(),
@@ -58,9 +70,9 @@ fn build_ui() -> impl Widget<GrabData> {
         rgba_image.clone().width() as usize,
         rgba_image.clone().height() as usize,
     );
-    let image = Image::new(image_buf);*/
-
-    let save_button = Button::new("Save").on_click(move |_ctx, _data: &mut GrabData ,_env| {
+    let image = Image::new(image_buf);
+    let image = ScreenshotWidget;*/
+    /*let save_button = Button::new("Save").on_click(move |_ctx, _data: &mut GrabData ,_env| {
         if !_data.image_data.is_empty() {
             fs::write(format!("Screen{}.{}",_data.screenshot_number,_data.save_format), _data.image_data.clone()).unwrap();
         }
@@ -76,9 +88,9 @@ fn build_ui() -> impl Widget<GrabData> {
     let cancel_button = Button::new("Cancel").on_click(move |_ctx, _data: &mut GrabData ,_env| {
         // cancel all image data
         _data.image_data = vec![];
-    });
+    });*/
 
-    Flex::column().with_child(create_monitor_buttons()).with_child(Flex::row().with_child(save_button).with_child(cancel_button))
+    Flex::column().with_child(create_monitor_buttons())
 }
 
 fn main() -> Result<(), PlatformError> {
@@ -87,13 +99,16 @@ fn main() -> Result<(), PlatformError> {
         .window_size((400.0, 300.0));
 
     let file = File::open("settings.json").unwrap();
-    let data: GrabData = from_reader(file).unwrap();
-    /*let data = GrabData {
+    //let data: GrabData = from_reader(file).unwrap();
+    let data = GrabData {
         screenshot_number: 1,
+        monitor_index: 0,
         image_data: vec![],
-        save_path: "C:\\Users\\Domenico\\Desktop".to_string(),
-        save_format: "png".to_string()
-    };*/
+        save_path: Path::new("C:\\Users\\Domenico\\Desktop").to_path_buf().into_boxed_path(),
+        save_format: "png".to_string(),
+        press: false,
+        positions: vec![],
+    };
 
     AppLauncher::with_window(main_window).delegate(Delegate).launch(data)
 }
@@ -109,9 +124,10 @@ impl AppDelegate<GrabData> for Delegate {
         _data: &mut GrabData,
         _env: &druid::Env,
     ) -> druid::Handled {
-        if cmd.is(druid::commands::CLOSE_WINDOW) {
+        if cmd.is(commands::CLOSE_WINDOW) {
             // Handle the window close event
             println!("Closing the window");
+            // cancel all image data
             _data.image_data = vec![];
             let file = File::create("settings.json").unwrap();
             to_writer(file, _data).unwrap();
